19796c964d458a7fb96ecf4fa88eaf097aa56468
Ronnie Villarini <ronnie@ronini.dev>
2024-01-20 11:49:01 -0600

    update header

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
     <main className="min-h-screen grid place-items-center p-24">
       <div className="banner">
         <h1 className="text-9xl coming-soon" aria-hidden="true">
-          Coming Soon
+          Web Idea Forge
         </h1>
-        <h1 className="text-9xl coming-soon">Coming Soon</h1>
+        <h1 className="text-9xl coming-soon">Web Idea Forge</h1>
       </div>
     </main>
   );
