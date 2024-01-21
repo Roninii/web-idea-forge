1a44f76b129f9416a76d36cc9202a0bbaad0722c
Ronnie Villarini <ronnie@ronini.dev>
2024-01-21 16:14:26 -0600

    heading style tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col justify-center items-center p-24">
       <div className="banner">
-        <h1 className="text-6xl lg:text-9xl coming-soon" aria-hidden="true">
+        <h1
+          className="text-6xl lg:text-9xl font-medium coming-soon"
+          aria-hidden="true"
+        >
           Web Idea Forge
         </h1>
         <h1 className="text-6xl lg:text-9xl text-center coming-soon">
