f537854885a83aac971452c527571c0d7cd54194
Ronnie Villarini <ronnie@ronini.dev>
2024-01-20 22:30:23 -0600

    add link to project detail page

--- app/ProjectIdea.tsx
+++ app/ProjectIdea.tsx
@@ -1,13 +1,20 @@
 import PROJECT_IDEAS from "./ideas.json";
+import Link from "next/link";
 
 export default function ProjectIdea() {
   const { ideas } = PROJECT_IDEAS;
   const randomIdea = ideas[Math.floor(Math.random() * ideas.length)];
   return (
-    <div className="mt-8">
+    <div className="mt-8 grid gap-4">
       <h2 className="text-center text-xl lg:text-4xl">
         {randomIdea.description_short}
       </h2>
+      <Link
+        href={`/idea/${randomIdea.id}`}
+        className="text-gray-400 text-center text-lg lg:text-xl hover:text-gradient"
+      >
+        More Info
+      </Link>
     </div>
   );
 }
