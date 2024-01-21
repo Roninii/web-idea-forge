13f33a699ba9ceb2af38065d6da0663ae48dee07
Ronnie Villarini <ronnie@ronini.dev>
2024-01-20 22:46:26 -0600

    add idea detail page

--- app/idea/[id]/page.tsx
+++ app/idea/[id]/page.tsx
@@ -1,3 +1,33 @@
-export default function ProjectDetails({ params }: { params: { id: number } }) {
-  return <h1>Project: {params.id} </h1>;
+import PROJECT_IDEAS from "@/app/ideas.json";
+
+export default function ProjectDetails({ params }: { params: { id: string } }) {
+  const { ideas } = PROJECT_IDEAS;
+  const idea = ideas.find((idea) => idea.id === parseInt(params.id));
+
+  return (
+    <>
+      <h1>{idea?.description_short}</h1>
+      <p>{idea?.description_long}</p>
+      {idea?.recommended_stack.length ? (
+        <>
+          <h2>Recommended Stack</h2>
+          <ul>
+            {idea?.recommended_stack.map((stackItem) => (
+              <li key={stackItem}>{stackItem}</li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+      {idea?.tags.length ? (
+        <>
+          <h2>Tags</h2>
+          <ul>
+            {idea?.tags.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        </>
+      ) : null}
+    </>
+  );
 }
