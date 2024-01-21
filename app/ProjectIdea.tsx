e709ad917bb952d0c12c14f20c16c54964d77453
Ronnie Villarini <ronnie@ronini.dev>
2024-01-20 21:59:46 -0600

    display random idea

--- app/ProjectIdea.tsx
+++ app/ProjectIdea.tsx
@@ -1,8 +1,12 @@
+import PROJECT_IDEAS from "./ideas.json";
+
 export default function ProjectIdea() {
+  const { ideas } = PROJECT_IDEAS;
+  const randomIdea = ideas[Math.floor(Math.random() * ideas.length)];
   return (
     <div className="mt-8">
-      <h2 className="text-center text-xl">
-        Build a site that displays a random project idea for a visitor to build.
+      <h2 className="text-center text-xl lg:text-4xl">
+        {randomIdea.description_short}
       </h2>
     </div>
   );
