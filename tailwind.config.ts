90cd6b8a311e1e1aa9fc62b72a8b2cb86a324076
Ronnie Villarini <ronnie@ronini.dev>
2024-01-21 22:57:59 -0600

    add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        redOrange: "#ff4500",
+        gold: "#ffd700",
+      },
     },
   },
   plugins: [
