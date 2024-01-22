a36da2977187af2295639c4cd360347970ebdc34
Ronnie Villarini <ronnie@ronini.dev>
2024-01-22 15:09:22 -0600

    disble sentry in dev

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,12 +4,13 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://1100bc2627add7c4c8ffd1354f46e723@o4506611323437056.ingest.sentry.io/4506611325861888",
+if (process.env.NODE_ENV !== "development")
+  Sentry.init({
+    dsn: "https://1100bc2627add7c4c8ffd1354f46e723@o4506611323437056.ingest.sentry.io/4506611325861888",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
