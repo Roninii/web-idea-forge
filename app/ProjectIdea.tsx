6ab0562791784f415add63441f3cfbba6f4a422c
Ronnie Villarini <ronnie@ronini.dev>
2024-01-24 12:02:34 -0600

    add ability to get new project idea

--- app/ProjectIdea.tsx
+++ app/ProjectIdea.tsx
@@ -1,20 +1,38 @@
+"use client";
+import { useState } from "react";
 import PROJECT_IDEAS from "./ideas.json";
 import Link from "next/link";
 
 export default function ProjectIdea() {
   const { ideas } = PROJECT_IDEAS;
   const randomIdea = ideas[Math.floor(Math.random() * ideas.length)];
+  const [idea, setIdea] = useState(randomIdea);
+
+  const newIdea = () => {
+    const newRandomIdea = ideas[Math.floor(Math.random() * ideas.length)];
+    setIdea(newRandomIdea);
+  };
+
   return (
     <div className="mt-8 grid gap-4">
       <h2 className="text-center text-xl lg:text-4xl">
-        {randomIdea.description_short}
+        {idea.description_short}
       </h2>
-      <Link
-        href={`/idea/${randomIdea.id}`}
-        className="text-gray-400 text-center text-lg lg:text-xl hover:text-gradient"
-      >
-        More Info
-      </Link>
+      <div className="flex justify-center items-center gap-4">
+        <Link
+          href={`/idea/${idea.id}`}
+          className="text-gray-400 text-center text-lg lg:text-xl hover:text-gradient"
+        >
+          More Info
+        </Link>
+        |
+        <button
+          onClick={newIdea}
+          className="text-gray-400 text-center text-lg lg:text-xl hover:text-gradient"
+        >
+          New Idea
+        </button>
+      </div>
     </div>
   );
 }
