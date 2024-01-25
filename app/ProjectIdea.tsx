08d809cbfcb24e4873aeb62c49590298171282ea
Ronnie Villarini <ronnie@ronini.dev>
2024-01-24 18:50:20 -0600

    update get new idea function

--- app/ProjectIdea.tsx
+++ app/ProjectIdea.tsx
@@ -7,9 +7,20 @@ export default function ProjectIdea() {
   const { ideas } = PROJECT_IDEAS;
   const randomIdea = ideas[Math.floor(Math.random() * ideas.length)];
   const [idea, setIdea] = useState(randomIdea);
+  const [previousIdeas, setPreviousIdeas] = useState<(typeof idea)[]>([]);
 
   const newIdea = () => {
-    const newRandomIdea = ideas[Math.floor(Math.random() * ideas.length)];
+    // Track previously shown ideas to avoid showing the same idea again.
+    setPreviousIdeas([...previousIdeas, idea]);
+    if (previousIdeas.length === ideas.length) setPreviousIdeas([]);
+
+    // Skip filter if previousIdeas is empty
+    const unusedIdeas = previousIdeas.length
+      ? ideas.filter((idea) => !previousIdeas.includes(idea))
+      : ideas;
+
+    const newRandomIdea =
+      unusedIdeas[Math.floor(Math.random() * unusedIdeas.length)];
     setIdea(newRandomIdea);
   };
 
