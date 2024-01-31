99938499516a1b05f0a64c7e69198c37168f7a0b
Ronnie Villarini <ronnie@ronini.dev>
2024-01-30 21:10:55 -0600

    add transition to idea detail page

---
+++ app/Transition.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { motion } from "framer-motion";
+
+export default function Transition({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <motion.div
+      initial={{ x: 300, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
--- app/idea/[id]/page.tsx
+++ app/idea/[id]/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import PROJECT_IDEAS from "@/app/ideas.json";
 import StackRecommendations from "@/app/idea/[id]/StackRecommendations";
 import TagList from "@/app/idea/[id]/TagList";
 import Actions from "./Actions";
+import Transition from "@/app/Transition";
 
 export default function ProjectDetails({ params }: { params: { id: string } }) {
   const { ideas } = PROJECT_IDEAS;
@@ -9,24 +11,26 @@ export default function ProjectDetails({ params }: { params: { id: string } }) {
 
   return (
     <main className="p-8">
-      <div className="max-w-screen-md mx-auto p-1 bg-gradient-to-r from-vermilion-500 to-gold-500 rounded-md">
-        <article className="bg-zinc-950 rounded p-8">
-          <h1 className="text-6xl text-gradient font-medium text-center mb-8">
-            {idea?.description_short}
-          </h1>
-          <p>{idea?.description_long}</p>
-          <StackRecommendations
-            className="mt-8"
-            recommended_stack={idea?.recommended_stack ?? []}
-          />
-          <TagList className="mt-8" tags={idea?.tags ?? []} />
-          <Actions
-            containerClasses="mt-8"
-            projectName={idea?.description_short ?? ""}
-            projectDescription={idea?.description_long ?? ""}
-          />
-        </article>
-      </div>
+      <Transition>
+        <div className="max-w-screen-md mx-auto p-1 bg-gradient-to-r from-vermilion-500 to-gold-500 rounded-md">
+          <article className="bg-zinc-950 rounded p-8">
+            <h1 className="text-6xl text-gradient font-medium text-center mb-8">
+              {idea?.description_short}
+            </h1>
+            <p>{idea?.description_long}</p>
+            <StackRecommendations
+              className="mt-8"
+              recommended_stack={idea?.recommended_stack ?? []}
+            />
+            <TagList className="mt-8" tags={idea?.tags ?? []} />
+            <Actions
+              containerClasses="mt-8"
+              projectName={idea?.description_short ?? ""}
+              projectDescription={idea?.description_long ?? ""}
+            />
+          </article>
+        </div>
+      </Transition>
     </main>
   );
 }
