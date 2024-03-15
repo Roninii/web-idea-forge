feeb036aedce52b9a0f649a15ee354b70e417cfe
Ronnie Villarini <ronnie@ronini.dev>
2024-03-15 11:57:04 -0500

    update more info styling

--- app/ProjectIdea.tsx
+++ app/ProjectIdea.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import PROJECT_IDEAS from "./ideas.json";
 import Link from "next/link";
+import { VscArrowSmallRight } from "react-icons/vsc";
 
 type Idea = {
   id: number;
@@ -38,33 +39,36 @@ export default function ProjectIdea() {
       {idea ? (
         <div className="banner">
           <h2
-            className="text-center text-6xl lg:text-8xl glow"
+            className="glow text-center text-6xl lg:text-8xl"
             aria-hidden="true"
           >
             {idea.description_short}
           </h2>
-          <h2 className="text-center text-6xl lg:text-8xl glow">
+          <h2 className="glow text-center text-6xl lg:text-8xl">
             {idea.description_short}
           </h2>
         </div>
       ) : (
         <h2 className="text-center text-xl lg:text-4xl">Forging...</h2>
       )}
-      <div className="flex justify-center items-center gap-4">
+      <div className="flex justify-center">
         <Link
           href={`/idea/${idea?.id}`}
-          className="text-gray-400 text-center text-lg lg:text-xl hover:text-gradient"
+          className="group relative px-8 py-4 text-2xl lg:text-3xl"
         >
-          More Info
+          <span className="group-hover:text-gradient text-center text-gray-400">
+            More Info
+          </span>
+          <VscArrowSmallRight className="absolute right-0 top-1/2 ml-2 -translate-y-1/2  transform opacity-0 transition-all duration-300 ease-out group-hover:inline group-hover:translate-x-2 group-hover:opacity-100" />
+          <hr className="from-vermilion-500 to-gold-500 mx-auto h-0.5 transform border-0 bg-gradient-to-r opacity-0 duration-300 group-hover:translate-y-1 group-hover:opacity-100" />
         </Link>
-        |
-        <button
-          onClick={newIdea}
-          className="text-gray-400 text-center text-lg lg:text-xl hover:text-gradient"
-        >
-          New Idea
-        </button>
       </div>
+      <button
+        onClick={newIdea}
+        className="hover:text-gradient mt-8 text-center text-lg text-gray-400 lg:text-xl"
+      >
+        New Idea
+      </button>
     </div>
   );
 }
